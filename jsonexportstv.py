6c6fe201e667935c77753af3af478f34ef1354f0
x02sstrikewyvern <160400677+x02sstrikewyvern@users.noreply.github.com>
2024-02-18 14:26:19 +0200

    Add files via upload

---
+++ jsonexportstv.py
@@ -0,0 +1,71 @@
+import random, json
+SEATS = 4
+
+candidates = ["a", "b", "c", "d", "e", "f"]
+
+file = open("votes.json", "r")
+inputBallots = json.loads(file.readline())
+file.close()
+
+ballots = inputBallots.copy()
+
+candidateVotes = {}
+for i in candidates:
+    candidateVotes[i] = 0
+
+quota = len(ballots) // len(candidates)
+elected = []
+currentVotes = []
+emptyBallots = 0
+
+quotaNotReached = False
+while len(elected) < SEATS:
+    for i in ballots:
+        try:
+            currentVotes.append(i[0])
+        except:
+            emptyBallots += 1
+    if len(ballots) == emptyBallots:
+        quotaNotReached = True
+        break
+
+    for i in candidates:
+        candidateVotes[i] = 0
+    for i in currentVotes:
+        candidateVotes[i] += 1
+    
+    for i in candidateVotes:
+        if i > quota:
+            # elect candidate
+            
+            elected.append(highestCandidate)
+
+            # transfer vote after election, prepared for next round
+            
+            for i in ballots:
+                if highestCandidate in i:
+                    i.remove(highestCandidate)
+
+    else:
+        # there are no votes to transfer, eliminate least popular candidate and transfer their votes (next round)
+        lowestCandidate = min(candidateVotes, key=candidateVotes.get)
+        for i in ballots:
+            if lowestCandidate in i:
+                i.remove(lowestCandidate)
+else:
+    if quotaNotReached:
+        while len(elected) < SEATS:
+            highestCandidate = max(candidateVotes, key=candidateVotes.get)
+            # elect candidate
+            
+            elected.append(highestCandidate)
+
+            # transfer vote after election, prepared for next round
+            
+            for i in ballots:
+                if highestCandidate in i:
+                    i.remove(highestCandidate)
+
+file = open("seats.json", "w")
+file.write(json.dumps(elected))
+file.close()
